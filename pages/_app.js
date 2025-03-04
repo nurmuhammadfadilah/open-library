870ab4a5d92779091ece2f7ecea5ff79a59c843d
nurmuhammadfadilah <muhammfadilah7@gmail.com>
2025-03-04 14:51:13 +0700

    update title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '@/styles/globals.css'; // Impor Tailwind global styles
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Open Library - Temukan Buku Favoritmu</title>
+        <meta name="description" content="Cari dan temukan buku favoritmu dengan mudah di Open Library." />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
